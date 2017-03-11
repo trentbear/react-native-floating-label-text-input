c0156d956d2ae2a8aabe9ff04f0f78fd863f92d1
Jay Huang <jay@machobear.ca>
2017-03-10 18:15:36 -0800

    Removed unnecessary view padding, extended styles to allow override, added offset for floating label

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+
 import {
   StyleSheet,
   Text,
@@ -9,14 +10,14 @@ import {
 
 class FloatingLabel extends Component {
   constructor(props) {
-    super(props);
+    super(props)
 
-    let initialPadding = 9;
-    let initialOpacity = 0;
+    let initialPadding = 9
+    let initialOpacity = 0
 
     if (this.props.visible) {
-      initialPadding = 5;
-      initialOpacity = 1;
+      initialPadding = 5
+      initialOpacity = 1
     }
 
     this.state = {
@@ -29,12 +30,12 @@ class FloatingLabel extends Component {
     Animated.timing(this.state.paddingAnim, {
       toValue: newProps.visible ? 5 : 9,
       duration: 230
-    }).start();
+    }).start()
 
     return Animated.timing(this.state.opacityAnim, {
       toValue: newProps.visible ? 1 : 0,
       duration: 230
-    }).start();
+    }).start()
   }
 
   render() {
@@ -42,13 +43,13 @@ class FloatingLabel extends Component {
       <Animated.View style={[styles.floatingLabel, { paddingTop: this.state.paddingAnim, opacity: this.state.opacityAnim }]}>
         {this.props.children}
       </Animated.View>
-    );
+    )
   }
 }
 
 class TextFieldHolder extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       marginAnim: new Animated.Value(this.props.withValue ? 10 : 0)
     }
@@ -58,7 +59,7 @@ class TextFieldHolder extends Component {
     return Animated.timing(this.state.marginAnim, {
       toValue: newProps.withValue ? 10 : 0,
       duration: 230
-    }).start();
+    }).start()
   }
 
   render() {
@@ -66,17 +67,17 @@ class TextFieldHolder extends Component {
       <Animated.View style={{ marginTop: this.state.marginAnim }}>
         {this.props.children}
       </Animated.View>
-    );
+    )
   }
 }
 
 class FloatLabelTextField extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       focused: false,
       text: this.props.value
-    };
+    }
   }
 
   componentWillReceiveProps(newProps) {
@@ -87,7 +88,7 @@ class FloatLabelTextField extends Component {
 
   withBorder() {
     if (!this.props.noBorder) {
-      return styles.withBorder;
+      return styles.withBorder
     }
   }
 
@@ -96,89 +97,93 @@ class FloatLabelTextField extends Component {
       <View style={styles.container}>
         <View style={styles.viewContainer}>
           <View style={styles.paddingView} />
-          <View style={[styles.fieldContainer, this.withBorder()]}>
+          <View
+            style={{
+              ...this.props.style,
+              ...styles.fieldContainer,
+              ...this.withBorder() }}
+          >
             <FloatingLabel visible={this.state.text}>
               <Text style={[styles.fieldLabel, this.labelStyle()]}>{this.placeholderValue()}</Text>
             </FloatingLabel>
             <TextFieldHolder withValue={this.state.text}>
-              <TextInput {...this.props}
-                ref='input'
+              <TextInput
+                {...this.props}
+                ref="input"
                 style={[styles.valueText]}
                 defaultValue={this.props.defaultValue}
-                value={this.state.text}
                 maxLength={this.props.maxLength}
                 onFocus={() => this.setFocus()}
                 onBlur={() => this.unsetFocus()}
-                onChangeText={(value) => this.setText(value)}
-                />
+              />
             </TextFieldHolder>
           </View>
         </View>
       </View>
-    );
+    )
   }
 
   inputRef() {
-    return this.refs.input;
+    return this.refs.input
   }
 
   focus() {
-    this.inputRef().focus();
+    this.inputRef().focus()
   }
 
   blur() {
-    this.inputRef().blur();
+    this.inputRef().blur()
   }
 
   isFocused() {
-    return this.inputRef().isFocused();
+    return this.inputRef().isFocused()
   }
 
   clear() {
-    this.inputRef().clear();
+    this.inputRef().clear()
   }
 
   setFocus() {
     this.setState({
       focused: true
-    });
+    })
     try {
-      return this.props.onFocus();
+      return this.props.onFocus()
     } catch (_error) { }
   }
 
   unsetFocus() {
     this.setState({
       focused: false
-    });
+    })
     try {
-      return this.props.onBlur();
+      return this.props.onBlur()
     } catch (_error) { }
   }
 
   labelStyle() {
     if (this.state.focused) {
-      return styles.focused;
+      return styles.focused
     }
   }
 
   placeholderValue() {
     if (this.state.text) {
-      return this.props.placeholder;
+      return this.props.placeholder
     }
   }
 
   setText(value) {
     this.setState({
       text: value
-    });
+    })
     try {
-      return this.props.onChangeTextValue(value);
+      return this.props.onChangeTextValue(value)
     } catch (_error) { }
   }
 }
 
-const styles = StyleSheet.create({
+const styles = {
   container: {
     flex: 1,
     height: 45,
@@ -189,13 +194,10 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'row'
   },
-  paddingView: {
-    width: 15
-  },
   floatingLabel: {
     position: 'absolute',
     top: 0,
-    left: 0
+    left: 10
   },
   fieldLabel: {
     height: 10,
@@ -217,8 +219,8 @@ const styles = StyleSheet.create({
     color: '#111111'
   },
   focused: {
-    color: "#1482fe"
+    color: '#1482fe'
   }
-});
+}
 
-export default FloatLabelTextField;
+export default FloatLabelTextField
